6e0ae4e0e09d6becf81722fa7f432067e7d1d1f7
hanx <763877917@qq.com>
2019-08-12 22:52:36 +0800

    doctor list

--- README.md
+++ README.md
@@ -9,3 +9,4 @@ html/user/reg.html 注册
 html/user/register-agreement.html  注册-用户协议
 html/asset-index.html//个人中心
 html/advice-list.html//咨询列表-我的咨询
+html/doctor-list.html//医生列表
---
+++ js/pages/doctor-list.js
@@ -0,0 +1,53 @@
+var Vue = require("vue");
+var $ = require("jquery");
+var C = require("common");
+
+var index_vm = new Vue({
+  el: '#advice-list-container',
+  data: {
+    tab1Flag: true,//tab切换
+    doctorList: [{
+      name:'赵医生',
+      serviceType: 1,//图文咨询
+      doctorPosition: "主任医师",
+      department: "皮肤科",
+      hospital: "合肥市蜀山区XX医院",
+      desc: "擅长：皮肤性病、乳腺病、痤疮、湿疹、瘙痒症、银屑 病、白癜风、黄褐斑、乳腺增生、带状孢疹、荨麻疹"
+    },{
+      name:'赵医生',
+      serviceType: 2,//图文咨询
+      doctorPosition: "主任医师",
+      department: "皮肤科",
+      hospital: "合肥市蜀山区XX医院",
+      desc: "擅长：皮肤性病、乳腺病、痤疮、湿疹、瘙痒症、银屑 病、白癜风、黄褐斑、乳腺增生、带状孢疹、荨麻疹"
+    },{
+      name:'赵医生',
+      serviceType: 1,//图文咨询
+      doctorPosition: "主任医师",
+      department: "皮肤科",
+      hospital: "合肥市蜀山区XX医院",
+      desc: "擅长：皮肤性病、乳腺病、痤疮、湿疹、瘙痒症、银屑 病、白癜风、黄褐斑、乳腺增生、带状孢疹、荨麻疹"
+    }],
+  },
+  methods: {
+    sendSms: function() {
+     
+    },
+    reg: function() {
+      var vm = this;
+      C.myAjax({
+        url: vm.regApiUrl,
+        type : "POST",
+        data: JSON.stringify({
+          aa:11
+        })
+      }, function(data) {
+        if(data.result) {
+        }
+      });
+    }
+  },
+  mounted: function() {
+
+  }
+});
